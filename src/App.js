726f516d474335de5549eb8633ba4bfa092697f1
Binyameen <bmghafoor@gmail.com>
2023-07-11 15:13:47 -0400

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import NavBar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
